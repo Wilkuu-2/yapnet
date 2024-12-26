84ed495dddc8d29da9f33d24f63b395d6df3e9bf
Jakub Stachurski <j.stachurski@student.utwente.nl>
2024-12-26 20:06:30 +0100

    Remove old messages

--- yapnet_core/src/protocol/mod.rs
+++ yapnet_core/src/protocol/mod.rs
@@ -12,7 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-pub mod message;
 use serde::{Deserialize, Serialize};
 pub mod body;
 
